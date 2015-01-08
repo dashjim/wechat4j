f9ecff09d2766533783ca436c36ba8f1f07749b6
Yummy <Yummy_Chou@163.com>
2015-01-08 20:18:25 +0800

    wechat4j by yummy

--- src/main/java/com/wisedu/wechat4jv2/api/WechatSupport.java
+++ src/main/java/com/wisedu/wechat4jv2/api/WechatSupport.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 
 public interface WechatSupport extends
         GroupSupport,
-        MenuSupport {
+        MenuSupport,
+        AuthorizationCodeSupport {
     void setOAuthAppId(String appId, String appSecret);
 
     void setAuthAppAccessToken(String accessToken, Long expiresIn);
