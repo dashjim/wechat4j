43bb8b0b3b2e31c22ea183098114edc5c012e352
Yummy <Yummy_Chou@163.com>
2015-01-16 09:13:11 +0800

    wechat4j by yummy

--- src/test/java/com/wisedu/wechat4jv2/examples/group/listGroup.java
+++ src/test/java/com/wisedu/wechat4jv2/examples/group/listGroup.java
@@ -7,11 +7,8 @@ import com.wisedu.wechat4jv2.entity.ResponseGroupCollection;
 import java.io.IOException;
 
 public class listGroup {
-    private static final String credential
-            = "XPke0AwiC6-CeSWuPsGV8lQcpCXDhilPv0mm-nr79vNSG7ciUZKNuiLCcj3x2CaDUAtq142EgU5F_HQ-73RSgVMNpDZ81B9a6u32sNDtPiE";
     public static void main(String[] args) throws IOException {
         Wechat wechat = new WechatFactory().getInstance();
-        wechat.setAccessToken(credential, null);
         ResponseGroupCollection collection = wechat.listGroup();
         if (collection.getResponse().getErrCode() == null){
             System.out.println("List Group Succeed." + collection);
