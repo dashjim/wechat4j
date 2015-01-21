18a0f48ae77b9b8138716be0473279e6a7faa261
Administrator <Yummy_Chou@163.com>
2015-01-21 22:37:11 +0800

    wechat4j by yummy

---
+++ src/test/java/com/wisedu/wechat4j/examples/group/listGroup.java
@@ -0,0 +1,17 @@
+package com.wisedu.wechat4j.examples.group;
+
+import com.wisedu.wechat4jv2.api.Wechat;
+import com.wisedu.wechat4jv2.client.WechatFactory;
+import com.wisedu.wechat4jv2.entity.ResponseGroupCollection;
+
+public class ListGroup {
+    public static void main(String[] args) throws Exception {
+        Wechat wechat = new WechatFactory().getInstance();
+        ResponseGroupCollection collection = wechat.listGroup();
+        if (collection.getResponse().getErrCode() == 0){
+            System.out.println("List Group Succeed." + collection);
+        } else {
+            System.err.println("List Group Failed." + collection);
+        }
+    }
+}
