917915ae6e386383b022daf1897f2f083fafa3c7
Yummy <Yummy_Chou@163.com>
2015-03-05 11:39:26 +0800

    wechat4j by yummy

--- src/main/java/com/wisedu/wechat4jv2/api/KFAccountService.java
+++ src/main/java/com/wisedu/wechat4jv2/api/KFAccountService.java
@@ -9,6 +9,9 @@ public interface KfAccountService {
     // 添加客服帐号
     Response createKfAccount(Map<String, Object> kfAccount) throws IOException;
 
+    // 获取所有客服账号
+    Response listKfAccount() throws IOException;
+
     // 客服接口-发消息
     Response sendMessage(Map<String, Object> message) throws IOException;
 }
