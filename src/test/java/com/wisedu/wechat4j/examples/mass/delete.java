a1031dc8340df3b41be5f6e2c9bbd385c3a65c5a
Administrator <Yummy_Chou@163.com>
2015-03-25 23:42:27 +0800

    commit

---
+++ src/test/java/com/wisedu/wechat4j/examples/mass/delete.java
@@ -0,0 +1,11 @@
+package com.wisedu.wechat4j.examples.mass;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 15-3-25
+ * Time: 下午11:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class delete {
+}
